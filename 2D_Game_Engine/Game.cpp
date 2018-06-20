2cb686c5bed3281d80ffc365966b6362afd7848f
AndyGJorgensen <AndyGJorgensen@gmail.com>
2018-06-19 20:30:50 -0600

    Sprite Class Ep8

--- 2D_Game_Engine/Game.cpp
+++ 2D_Game_Engine/Game.cpp
@@ -1,19 +1,15 @@
 #include "Game.h"
 #include "TextureManager.h"
-#include "GameObject.h"
 #include "Map.h"
-
-#include "ECS.h"
 #include "Components.h"
 
-GameObject* player;
-GameObject* enemy;
+
 Map* map;
+Manager manager;
 
 SDL_Renderer* Game::renderer = nullptr;
 
-Manager manager;
-auto& newplayer(manager.addEntity());
+auto& player(manager.addEntity());
 
 Game::Game()
 {}
@@ -42,11 +38,13 @@ void Game::init(const char* title, int width, int height, bool fullscreen)
 		isRunning = true;
 	}
 
-	player = new GameObject("player.png", 0, 0);
-	enemy = new GameObject("enemy.png", 50, 10);
 	map = new Map();
-	newplayer.addComponent<PositionComponent>();
-	newplayer.getComponent<PositionComponent>().setPos(250, 250);
+
+	player.addComponent<PositionComponent>(10,20);
+	player.addComponent<SpriteComponent>("player.png");
+
+
+
 }
 
 void Game::handleEvents()
@@ -67,21 +65,23 @@ void Game::handleEvents()
 
 void Game::update()
 {
-	//cnt++;
-	player->Update();
-	enemy->Update();
+
+	manager.refresh();
 	manager.update();
-	//std::cout << cnt << std::endl;
-	std::cout << newplayer.getComponent<PositionComponent>().x() << ","<<
-		newplayer.getComponent<PositionComponent>().x() <<std::endl;
+
+	if (player.getComponent<PositionComponent>().x() > 200)
+	{
+		player.getComponent<SpriteComponent>().setTex("enemy.png");
+
+	}
+
 }
 
 void Game::render()
 {
 	SDL_RenderClear(renderer);
 	map->DrawMap();
-	player->Render();
-	enemy->Render();
+	manager.draw();
 	SDL_RenderPresent(renderer);
 }
 
